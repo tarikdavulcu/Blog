5bd278f6823a8321a4d3b1066dd3512ccda443f8
Tarik Davulcu <tarik.davulcu@asay.com.tr>
2023-10-30 05:14:43 +0300

    email send updated

--- myblogNew/Controllers/BlogController.cs
+++ myblogNew/Controllers/BlogController.cs
@@ -10,36 +10,37 @@ using System.Net.Mail;
 using System.ServiceModel.Syndication;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace myblogNew.Controllers
 {
-  public class BlogController : MyController
-  {
-    myBlogContext context = new myBlogContext();
-    // GET: Blog
-
-    public ActionResult Index(string id)
+    public class BlogController : MyController
     {
-      ViewBag.ayarlar = context.SiteAyarlaris.ToList();
+        myBlogContext context = new myBlogContext();
+        // GET: Blog
 
-      var data = context.Blogs.OrderByDescending(i=>i.BlogID).ToList();
+        public ActionResult Index(string id)
+        {
+            ViewBag.ayarlar = context.SiteAyarlaris.ToList();
 
-      if (id != null && id != "All")
-      {
-        data = context.Blogs.Where(p => p.Kullanicilar.KullaniciAdi == id).OrderByDescending(i => i.BlogID).ToList();
-      }
-      else if (id == null)
-      {
-        data = context.Blogs.OrderByDescending(i => i.BlogID).ToList();
+            var data = context.Blogs.OrderByDescending(i => i.BlogID).ToList();
+
+            if (id != null && id != "All")
+            {
+                data = context.Blogs.Where(p => p.Kullanicilar.KullaniciAdi == id).OrderByDescending(i => i.BlogID).ToList();
             }
-      else if (id == "All")
-      {
-        data = context.Blogs.OrderByDescending(i => i.BlogID).ToList();
+            else if (id == null)
+            {
+                data = context.Blogs.OrderByDescending(i => i.BlogID).ToList();
             }
-      return View(data);
-    }
+            else if (id == "All")
+            {
+                data = context.Blogs.OrderByDescending(i => i.BlogID).ToList();
+            }
+            return View(data);
+        }
 
         [AcceptVerbs(HttpVerbs.Post | HttpVerbs.Get)]
 
@@ -50,150 +51,149 @@ namespace myblogNew.Controllers
         }
 
         public ActionResult Iletisim()
-    {
-      return View(context.SiteAyarlaris.ToList());
-    }
+        {
+            return View(context.SiteAyarlaris.ToList());
+        }
 
-    void MailGonderme(string body)
-    {
-      //    var kimden = new MailAddress("info@tarikdavulcu.com");
-      //    var kime = new MailAddress("info@tarikdavulcu.com");
-      //    const string konu = "Tarık Davulcu | Siteden Gelen | İletişim Formu";
-      //    using (var smtp = new SmtpClient
-      //    {
-      //        Host = "smtp.gmail.com",
-      //        Port = 465,
-      //        EnableSsl = true,
-      //        DeliveryMethod = SmtpDeliveryMethod.Network,
-      //        UseDefaultCredentials = false,
-      //        Credentials = new System.Net.NetworkCredential(kimden.Address, "20342034T-d")
-      //})
-      //{
-      //using (var message = new MailMessage(kimden, kime) { Subject = konu, Body = body })
-      //{
-      //    smtp.Send(message);
-      //}
-      //}
+        public string MailGondermeAsync(string body)
+        {
+            var kimden = new MailAddress("info@tarikdavulcu.com");
+            var kime = new MailAddress("info@tarikdavulcu.com");
+            const string konu = "Tarık Davulcu | Siteden Gelen | İletişim Formu";
+            using (var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new System.Net.NetworkCredential(kimden.Address, "dbjt gkbx addy pfpf")
+            })
+            {
+                using (var message = new MailMessage(kimden, kime) { Subject = konu, Body = body })
+                {
+                    try
+                    {
+                        smtp.Send(message);
+                        return "E-Posta gönderildi. En kısa sürede sizinle iletişime geçilecektir.";
 
-      var apiKey = "";
-      var client = new SendGridClient(apiKey);
-      var from = new EmailAddress("info@tarikdavulcu.com", "Star Web Design Studio");
-      var subject = "Tarık Davulcu | Siteden Gelen | İletişim Formu";
-      var to = new EmailAddress("info@tarikdavulcu.com", "Formu Dolduran Mail");
-      var plainTextContent = body;
-      var htmlContent = "";
-      var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-      var response = client.SendEmailAsync(msg);
-    }
-    public ActionResult Header()
-    {
-      return PartialView(context.SiteAyarlaris.ToList());
-    }
-    [HttpPost]
-    public ActionResult SendMail(FormCollection mesaj)
-    {
-      if (this.IsCaptchaValid("Captcha is not valid"))
-      {
-        //captcha verisi doğruysa yapılacaklar
-        Mesajlar m = new Mesajlar();
-        m.Ad = mesaj["name"].ToString();
-        m.Mail = mesaj["email"].ToString();
-        m.Mesaj = mesaj["message"].ToString();
-        m.Telefon = mesaj["phone"].ToString();
-        context.Mesajlars.Add(m);
-        context.SaveChanges();
+                    }
+                    catch (Exception ex)
+                    {
 
-        //mail gönderme işlemi yapacağız
-        var body = new StringBuilder();
-        body.AppendLine("Gönderen :" + mesaj["name"].ToString());
-        body.AppendLine("Mail Adresi :" + mesaj["email"].ToString());
-        body.AppendLine("Tel :" + mesaj["phone"].ToString());
-        body.AppendLine("Mesaj :" + mesaj["message"].ToString());
-        MailGonderme(body.ToString());
-        TempData["Ret"] = "E-Posta gönderildi. En kısa sürede sizinle iletişime geçilecektir.";
-        return RedirectToAction("Index", "Blog");
-      }
-      else
-      {
-        return RedirectToAction("Index", "Blog");
-      }
-    }
-    public ActionResult Title()
-    {
-      ViewBag.ayarlar = context.SiteAyarlaris.ToList();
-      return PartialView(ViewBag.ayarlar);
-    }
-    public ActionResult Menu()
-    {
-      ViewBag.ayarlar = context.SiteAyarlaris.ToList();
-      return PartialView((from i in context.Menus orderby i.SiraNo ascending select i).ToList());
-    }
+                        return "Error: " + ex.Message.ToString();
+                    }
+                }
+            }
+        }
+        public ActionResult Header()
+        {
+            return PartialView(context.SiteAyarlaris.ToList());
+        }
+        [HttpPost]
+        public ActionResult SendMail(FormCollection mesaj)
+        {
+            if (this.IsCaptchaValid("Captcha is not valid"))
+            {
+                //captcha verisi doğruysa yapılacaklar
+                Mesajlar m = new Mesajlar();
+                m.Ad = mesaj["name"].ToString();
+                m.Mail = mesaj["email"].ToString();
+                m.Mesaj = mesaj["message"].ToString();
+                m.Telefon = mesaj["phone"].ToString();
+                context.Mesajlars.Add(m);
+                context.SaveChanges();
 
-    public ActionResult Footer()
-    {
-      return PartialView(context.SiteAyarlaris.ToList());
-    }
+                //mail gönderme işlemi yapacağız
+                var body = new StringBuilder();
+                body.AppendLine("Gönderen :" + mesaj["name"].ToString());
+                body.AppendLine("Mail Adresi :" + mesaj["email"].ToString());
+                body.AppendLine("Tel :" + mesaj["phone"].ToString());
+                body.AppendLine("Mesaj :" + mesaj["message"].ToString());
+                TempData["Ret"] = MailGondermeAsync(body.ToString());
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                return RedirectToAction("Index", "Blog");
+            }
+        }
+        public ActionResult Title()
+        {
+            ViewBag.ayarlar = context.SiteAyarlaris.ToList();
+            return PartialView(ViewBag.ayarlar);
+        }
+        public ActionResult Menu()
+        {
+            ViewBag.ayarlar = context.SiteAyarlaris.ToList();
+            return PartialView((from i in context.Menus orderby i.SiraNo ascending select i).ToList());
+        }
 
+        public ActionResult Footer()
+        {
+            return PartialView(context.SiteAyarlaris.ToList());
+        }
 
 
 
 
-    public ActionResult Hakkimda()
-    {
-      return View(context.SiteAyarlaris.ToList());
-    }
 
-    public ActionResult Detayi(string id)
-    {
-      var r = context.Blogs.Where(p => p.SeoLink == id).ToList();
-      if (r.Count > 0)
-      {
-        ViewBag.Title = r[0].Baslik;
-        return View(r);
-      }
-      else return RedirectToAction("Error");
-    }
-    public ActionResult Error()
-    {
+        public ActionResult Hakkimda()
+        {
+            return View(context.SiteAyarlaris.ToList());
+        }
 
-      Response.StatusCode = 404;
-      Response.TrySkipIisCustomErrors = true;
-      return View();
+        public ActionResult Detayi(string id)
+        {
+            var r = context.Blogs.Where(p => p.SeoLink == id).ToList();
+            if (r.Count > 0)
+            {
+                ViewBag.Title = r[0].Baslik;
+                return View(r);
+            }
+            else return RedirectToAction("Error");
+        }
+        public ActionResult Error()
+        {
+
+            Response.StatusCode = 404;
+            Response.TrySkipIisCustomErrors = true;
+            return View();
         }
 
 
         public ActionResult RssFeed()
-    {
-      SyndicationFeed feed = null;
-      string siteTitle, description, siteUrl;
-      siteTitle = "Blog";
-      siteUrl = "https://www.tarikdavulcu.com";
-      description = "Welcome to the tarikdavulcu.com!";
-
-
-      List<SyndicationItem> items = new List<SyndicationItem>();
-      //Last 10 Post
-      var c = context.Blogs.OrderByDescending(a => a.BlogID).Take(10).ToList();
-      foreach (var i in c)
-      {
-        SyndicationItem item = new SyndicationItem
         {
-          Title = new TextSyndicationContent(i.Baslik),
-          Content = new TextSyndicationContent(GetPlainText(i.Icerik, 200)), //here content may be Html content so we should use plain text
-          PublishDate = DateTimeOffset.Parse(i.PostTarihi.ToString()),
-        };
-        item.Links.Add(new SyndicationLink(new Uri(Request.Url.Scheme + "://" + Request.Url.Host + i.SeoLink)));
-        items.Add(item);
-      }
-      feed = new SyndicationFeed(siteTitle, description, new Uri(siteUrl));
-      feed.Items = items;
-      return new RSSResult { feedData = feed };
+            SyndicationFeed feed = null;
+            string siteTitle, description, siteUrl;
+            siteTitle = "Blog";
+            siteUrl = "https://www.tarikdavulcu.com";
+            description = "Welcome to the tarikdavulcu.com!";
+
+
+            List<SyndicationItem> items = new List<SyndicationItem>();
+            //Last 10 Post
+            var c = context.Blogs.OrderByDescending(a => a.BlogID).Take(10).ToList();
+            foreach (var i in c)
+            {
+                SyndicationItem item = new SyndicationItem
+                {
+                    Title = new TextSyndicationContent(i.Baslik),
+                    Content = new TextSyndicationContent(GetPlainText(i.Icerik, 200)), //here content may be Html content so we should use plain text
+                    PublishDate = DateTimeOffset.Parse(i.PostTarihi.ToString()),
+                };
+                item.Links.Add(new SyndicationLink(new Uri(Request.Url.Scheme + "://" + Request.Url.Host + i.SeoLink)));
+                items.Add(item);
+            }
+            feed = new SyndicationFeed(siteTitle, description, new Uri(siteUrl));
+            feed.Items = items;
+            return new RSSResult { feedData = feed };
+        }
+        private string GetPlainText(string htmlContent, int length = 0)
+        {
+            string HTML_TAG_PATTERN = "<.*?>";
+            string plainText = Regex.Replace(htmlContent, HTML_TAG_PATTERN, string.Empty);
+            return length > 0 && plainText.Length > length ? plainText.Substring(0, length) : plainText;
+        }
     }
-    private string GetPlainText(string htmlContent, int length = 0)
-    {
-      string HTML_TAG_PATTERN = "<.*?>";
-      string plainText = Regex.Replace(htmlContent, HTML_TAG_PATTERN, string.Empty);
-      return length > 0 && plainText.Length > length ? plainText.Substring(0, length) : plainText;
-    }
-  }
 }
